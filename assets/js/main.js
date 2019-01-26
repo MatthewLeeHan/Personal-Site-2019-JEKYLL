9ed3aa9504d878f7b6a99509cff645cd6c822356
MatthewLeeHan <mlh6fc@virginia.edu>
2019-01-26 01:01:14 -0500

    hamburger press -> hide bg, need to figure out blur

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,11 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 // const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
-
+const rightSide = document.getElementById('right-side');
 // set initial state of my menu
 let showMenu = false; // using let because we want to directly reassign at times
-
+const paragraph = document.getElementById('paragraph');
+const linkHolder = document.getElementById('link-holder');
 menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if(!showMenu){
@@ -16,7 +17,10 @@ function toggleMenu() {
         menuNav.classList.add('show');
         // menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
-
+        // blur
+        rightSide.classList.add('moveBack');
+        paragraph.classList.add('moveBack');
+        linkHolder.classList.add('moveBack');
         // set menu state
         showMenu = true;
     } else {
@@ -28,5 +32,10 @@ function toggleMenu() {
 
         // set menu state
         showMenu = false;
+
+        //blur
+        rightSide.classList.remove('moveBack');
+        paragraph.classList.remove('moveBack');
+        linkHolder.classList.remove('moveBack');
     }
 }
